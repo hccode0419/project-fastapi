5f7cc75423a579c1a73c17f6f5a8e1bd13eb38d2
hccode0419 <67219037+hccode0419@users.noreply.github.com>
2024-10-15 09:14:20 +0900

    item_router.py 주석추가

--- item/item_router.py
+++ item/item_router.py
@@ -64,10 +64,25 @@ def update_item(item: Modify_item,
     modify_item = Modify_item(item_name=item.item_name,
                               item_price=item.item_price,
                               amount=item.amount)
-    
+
     item = item_db.query(Item_model).filter(Item_model.item_id == item_id).first()
+
+    """
+    item_db에서 Item_model에 해당하는 table을 들고온 후 Item_model.item_id와 입력한 item_id가 같은 첫번째 row를 들고온다.
+
+    이때 Item_model은 model.py에 작성한 DB table의 양식이다.
+
+    이 형식은 자주 등장할 것이니 기억해주세요!
+
+    """
+
     if not item:
         raise HTTPException(status_code=404, detail=f"item_id : {item_id} 내역이 없습니다.")
+
+    """
+    item이 있는지 확인하여 없으면 예외처리한다.
+
+    """
 
     item.item_name = modify_item.item_name
     item.item_price = modify_item.item_price
@@ -81,8 +96,10 @@ def update_item(item: Modify_item,
 @router.delete("/delete_item")
 def delete_item(item_id: int,
                 item_db: Session = Depends(get_itemdb)):
+    
+    
     item = item_db.query(Item_model).filter(Item_model.item_id == item_id)
-
+    
     if not item.first():
         raise HTTPException(status_code=404, detail=f"item_id : {item_id} 내역이 없습니다.")
     
