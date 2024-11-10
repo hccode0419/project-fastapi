0d2efac3c1687ec88f94be7738410e1787df33bd
hccode0419 <gmlcjf419@gmail.com>
2024-11-10 18:23:50 +0900

    learning 241108

--- item/item_schema.py
+++ item/item_schema.py
@@ -1,6 +1,10 @@
 from pydantic import BaseModel
+from datetime import datetime
 
-class create_model(BaseModel):
-    item_id:int
+# 입력 받는 양식
+class Create_item(BaseModel):
     item_name: str
     item_price: int
+    amount: int
+    create_at: str
+    create_date: datetime
