2bcdd953902db62282b3859fdb1b6ed757f70088
hccode0419 <gmlcjf419@gmail.com>
2024-11-19 14:45:02 +0900

    회원가입 구현

--- user/user_router.py
+++ user/user_router.py
@@ -22,11 +22,14 @@ def sign_up(user: User,
             user_db : Session = Depends(get_userdb)):
     
     # 아이디 중복 확인
+    check_user_id = user_db.query(User_model).filter(User_model.user_id == user.user_id).first()
+    if check_user_id:
+        raise HTTPException(status_code=404, detail="해당 아이디는 존재하는 아이디입니다.")
     
     # 전화번호 중복 확인
-    
-    
-    
+    check_phone_number = user_db.query(User_model).filter(User_model.phone_number == user.phone_number).first()
+    if check_phone_number:
+        raise HTTPException(status_code=404, detail="중복되는 전화번호입니다.")
     
     user_info = User_model(user_id = user.user_id,
                      password=user.password,
@@ -38,6 +41,7 @@ def sign_up(user: User,
     insert_data(user_db, user_info)
 
     return user_info
+
 @router.get("/log_in")
 def log_in():
     pass
